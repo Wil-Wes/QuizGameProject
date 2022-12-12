9cb91190ce9cc968604fdf524718d51f67c309dd
Wil-Wes <will.d.wes@gmail.com>
2022-12-12 08:52:50 -0600

    CompletedProject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Step 2: Import Data list and question model
 from question_model import Question
+from question_model import Question
 from data import question_data
 
 # Step 3 Create question bank using the question_data that has been imported (hint: loops)
@@ -30,3 +31,17 @@ while quiz.still_has_questions():
     quiz.next_question()
 
 print("You have completed the quiz!", f"Your final score was: {quiz.score}/{quiz.question_number}")
+
+# Step 6: Create new QuizBrain object using the QuizBrain Class and pass in the question_bank
+quiz = QuizBrain(question_bank)
+
+# 6a. Run the next_question() method on the object just created
+# quiz.next_question()
+
+# 7a. Create "game loop" (while loop) to run the game
+while quiz.still_has_questions():
+    # 7b. Move the next_question() method from step 6a into the while loop
+    quiz.next_question()
+
+# Step 9: Notify user when quiz is completed and show the final score
+print("You've completed the quiz!", f"Your final score was: {quiz.score}/{quiz.question_number}")
